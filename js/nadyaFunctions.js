c31ed078d5ba8377efaf5fda763cb4f1239694c5
nadyac <nadyacpena@gmail.com>
2016-09-23 07:39:44 -0400

    major overhaul of content. Deleted the project list, added more svgs and functions to manipulate them.

--- js/nadyaFunctions.js
+++ js/nadyaFunctions.js
@@ -1,5 +1,46 @@
+var clicked = false;
+var xyCoords;
+var id;
+
 function showCoords(event) {
-    var x = event.clientX;
+    xyCoords = getCoordsNoEvent();
+    var coords = "X coords: " + xyCoords[0] + ", Y coords: " + xyCoords[1];
+    document.getElementById("coordinates").innerHTML = coords;
+}
+
+function setClicked(){
+	if(clicked === false){
+		clicked = true;
+	} else{
+		clicked = false;
+	}
+}
+function changeXYPos(elemID){ 
+	setClicked();
+	if(clicked === true && typeof xyCoords != 'undefined' && xyCoords.length > 0){
+		id = elemID;
+		setInterval(function(){
+			var mouseX = xyCoords[0]-300;
+			var mouseY = xyCoords[1]-300;
+			if(typeof id != 'undefined' && id != null){
+				document.getElementById(id).setAttribute("cx", mouseX);
+				document.getElementById(id).setAttribute("cy", mouseY);
+			} 
+			var elemX = document.getElementById(id).getAttribute("cx");
+			var elemY = document.getElementById(id).getAttribute("cy");
+			var coords = "elemX: " + elemX + ", elemY: " + elemY;
+			document.getElementById("coordinatesTwo").innerHTML = coords;
+		}, 0.5);
+	}
+}
+
+function getCoordsWithEvent(event){
+	var x = event.clientX;
     var y = event.clientY;
-    var coords = "X coords: " + x + ", Y coords: " + y;
-    alert(coords);
+    return [x,y];
+}
+function getCoordsNoEvent(){
+	var x = event.clientX;
+    var y = event.clientY;
+    return [x,y];
+}
